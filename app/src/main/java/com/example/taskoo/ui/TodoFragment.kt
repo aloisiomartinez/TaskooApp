c674f9d424a82c0f42543a2262c7b4a42119ad55
aloisiomartinez <aloisiojacobs36@gmail.com>
2025-01-06 07:15:21 -0300

    Clicks events on RecyclerView

--- app/src/main/java/com/example/taskoo/ui/TodoFragment.kt
+++ app/src/main/java/com/example/taskoo/ui/TodoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.taskoo.R
@@ -44,21 +45,52 @@ class TodoFragment : Fragment() {
         }
     }
 
+
     private fun initRecyclerView(taskList: List<Task>) {
-        taskAdapter = TaskAdapter(requireContext(),taskList)
+        taskAdapter = TaskAdapter(requireContext(), taskList) { task, option ->
+            optionSelected(task, option)
+        }
 
         binding.rvTasks.layoutManager = LinearLayoutManager(requireContext())
         binding.rvTasks.setHasFixedSize(true)
         binding.rvTasks.adapter = taskAdapter
     }
 
+    private fun optionSelected(task: Task, option: Int) {
+        when (option) {
+            TaskAdapter.SELECT_REMOVE -> {
+                Toast.makeText(
+                    requireContext(),
+                    "Removendo ${task.description}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            TaskAdapter.SELECT_EDIT -> {
+                Toast.makeText(requireContext(), "Editando ${task.description}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            TaskAdapter.SELECT_DETAILS -> {
+                Toast.makeText(requireContext(), "Detalhes ${task.description}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            TaskAdapter.SELECT_NEXT -> {
+                Toast.makeText(requireContext(), "Next ${task.description}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+
     private fun getTasks() = listOf<Task>(
         Task("0", "Criar nova tela do App", Status.TODO),
-        Task("1", "Criar nova tela do App de Login",Status.TODO),
-        Task("2", "Criar nova tela do App de Rec Senha",Status.TODO),
-        Task("3", "Salvar task",Status.TODO),
-        Task("4", "Deletar Task",Status.TODO),
-        Task("5", "Criar conta",Status.TODO),
+        Task("1", "Criar nova tela do App de Login", Status.TODO),
+        Task("2", "Criar nova tela do App de Rec Senha", Status.TODO),
+        Task("3", "Salvar task", Status.TODO),
+        Task("4", "Deletar Task", Status.TODO),
+        Task("5", "Criar conta", Status.TODO),
     )
 
     override fun onDestroyView() {
