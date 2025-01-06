31197b96687b1615dcd0935fc97c04b0a0712c9e
aloisiomartinez <aloisiojacobs36@gmail.com>
2025-01-06 07:01:17 -0300

    task colors indicators with adapter

--- app/src/main/java/com/example/taskoo/ui/adapter/TaskAdapter.kt
+++ app/src/main/java/com/example/taskoo/ui/adapter/TaskAdapter.kt
@@ -1,13 +1,19 @@
 package com.example.taskoo.ui.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import com.example.taskoo.R
+import com.example.taskoo.data.model.Status
 import com.example.taskoo.data.model.Task
 import com.example.taskoo.databinding.ItemTaskBinding
 
 class TaskAdapter(
+    private val context: Context,
     private val taskList: List<Task>
 ) : RecyclerView.Adapter<TaskAdapter.MyViewHolder>() {
 
@@ -25,11 +31,41 @@ class TaskAdapter(
         val task = taskList[position]
 
         holder.binding.textDescription.text = task.description
+
+        setIndicators(task, holder)
+    }
+
+    private fun setIndicators(task: Task, holder: MyViewHolder) {
+        when (task.status) {
+            Status.TODO -> {
+                holder.binding.btnBack.isVisible = false
+            }
+
+            Status.DOING -> {
+                holder.binding.btnBack.setColorFilter(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.color_status_todo
+                    )
+                )
+
+                holder.binding.btnNext.setColorFilter(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.color_status_done
+                    )
+                )
+
+            }
+
+            Status.DONE -> {
+                holder.binding.btnNext.isVisible = false
+            }
+
+        }
     }
 
     override fun getItemCount() = taskList.size
 
     inner class MyViewHolder(val binding: ItemTaskBinding) : RecyclerView.ViewHolder(binding.root)
-
-
 }
